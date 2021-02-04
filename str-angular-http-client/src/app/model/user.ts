238a05128268afa4ccb2bbaa34bd79f2d3aa4220
Joe Cserko <j.cserko@gmail.com>
2021-02-04 18:04:41 +0100

    Feat: getAll.

---
+++ str-angular-http-client/src/app/model/user.ts
@@ -0,0 +1,11 @@
+// tslint:disable: variable-name
+// {"id":1000,"first_name":"Red","last_name":"Danford","email":"rdanfordrr@mozilla.org","gender":"Agender","ip_address":"73.31.248.104"}
+
+export class User {
+  id: number = 0;
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  gender: string = '';
+  ip_address: string = '';
+}
